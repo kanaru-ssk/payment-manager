04e796bc122b0f948cab1df0f97346ea4a934ff3
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-25 06:23:37 +0900

    リファクタ

--- backend/domains/user/repository.go
+++ backend/domains/user/repository.go
@@ -3,9 +3,9 @@ package user
 import "context"
 
 type UserRepository interface {
-	FindByUserID(ctx context.Context, userID string) (*User, error)
+	FindByUserId(ctx context.Context, userId string) (*User, error)
 
 	Create(ctx context.Context, user *User) (*User, error)
 	Update(ctx context.Context, user *User) (*User, error)
-	Delete(ctx context.Context, userID string) error
+	Delete(ctx context.Context, userId string) error
 }
