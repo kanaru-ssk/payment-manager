67141695fcbd407484377fbd9547cfa326e5c15c
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-26 01:08:40 +0900

    Cloud Runモジュール作成

---
+++ infra/modules/cloud_run_service/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type        = string
+  description = "Project name"
+}
+
+variable "project_id" {
+  type        = string
+  description = "Project id"
+}
+
+variable "region" {
+  type        = string
+  description = "region"
+}
