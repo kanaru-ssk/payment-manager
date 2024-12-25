790ec5ad6c1b01f718dc11a27bffd071550553fd
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-25 11:21:07 +0900

    godoc追加

--- backend/domains/user/repository.go
+++ backend/domains/user/repository.go
@@ -3,9 +3,23 @@ package user
 import "context"
 
 type UserRepository interface {
+	// FindByUserId : 指定されたuserIdに基づいてユーザーを取得
+	// userId: ユーザーID
+	// return: ユーザー情報、またはnil
 	FindByUserId(ctx context.Context, userId string) (*User, error)
 
-	Create(ctx context.Context, user *User) (*User, error)
-	Update(ctx context.Context, user *User) (*User, error)
-	Delete(ctx context.Context, userId string) error
+	// CreateUser : 新しいユーザーを作成
+	// user: ユーザー情報
+	// return: 作成されたユーザー情報
+	CreateUser(ctx context.Context, user *User) (*User, error)
+
+	// UpdateUser : ユーザー情報を更新
+	// user: 更新するユーザー情報
+	// return: 更新されたユーザー情報
+	UpdateUser(ctx context.Context, user *User) (*User, error)
+
+	// DeleteUser : 指定されたuserIdのユーザーを削除
+	// userId: ユーザーID
+	// return: 削除の成功状態
+	DeleteUser(ctx context.Context, userId string) error
 }
