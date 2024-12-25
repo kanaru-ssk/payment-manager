883d4110ed4acf44b5657b830ba22548990e9dfa
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-26 00:17:14 +0900

    artifact repository作成

--- infra/environments/common/main.tf
+++ infra/environments/common/main.tf
@@ -13,6 +13,14 @@ module "google_project" {
   group           = var.project_name
 }
 
+module "project_services" {
+  source     = "../../modules/project_services"
+  project_id = local.project_id
+  services = [
+    "artifactregistry.googleapis.com"
+  ]
+}
+
 resource "google_storage_bucket" "terraform_backend" {
   name          = "terraform-backend-${var.project_name}"
   location      = var.region
@@ -20,4 +28,15 @@ resource "google_storage_bucket" "terraform_backend" {
   force_destroy = true
 
   uniform_bucket_level_access = true
+}
+
+resource "google_artifact_registry_repository" "main" {
+  repository_id = "main"
+  location      = var.region
+  project       = local.project_id
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
 }
