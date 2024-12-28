00c890ad3818ba1b20745013047b08d36358400d
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-28 20:01:32 +0900

    grpcクライアントコード作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,35 @@
+import { findUserByUserId } from "@/infrastructure/user/repository";
+
 export default async function Home() {
-  if (process.env.BACKEND_URL === undefined) return null;
+  const user = await findUserByUserId("ad1b2ae0-30ac-818f-c44b-d041440369d5");
+  if (!user) return <div className="p-5">user not found</div>;
 
-  // backendと疎通するサンプル
-  console.log(process.env.BACKEND_URL);
-  const response = await fetch(process.env.BACKEND_URL);
-  const text = (await response.text()).toString();
-
-  return <div className="p-5">{text}</div>;
+  return (
+    <div className="p-5">
+      <table className="border text-left">
+        <tbody>
+          <tr>
+            <th className="border p-2">userId</th>
+            <td className="border p-2">{user.userId}</td>
+          </tr>
+          <tr>
+            <th className="border p-2">userName</th>
+            <td className="border p-2">{user.userName}</td>
+          </tr>
+          <tr>
+            <th className="border p-2">email</th>
+            <td className="border p-2">{user.email}</td>
+          </tr>
+          <tr>
+            <th className="border p-2">createdAt</th>
+            <td className="border p-2">{user.createdAt.toLocaleString()}</td>
+          </tr>
+          <tr>
+            <th className="border p-2">updatedAt</th>
+            <td className="border p-2">{user.updatedAt.toLocaleString()}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
