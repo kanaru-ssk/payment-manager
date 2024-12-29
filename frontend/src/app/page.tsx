db41f7eb0915c0875957fefab739993f86a2cbc4
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-29 12:56:53 +0900

    SSGをオフにする

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 import { findUserByUserId } from "@/infrastructure/user/repository";
 import { UserTable } from "./user-table";
+import { connection } from "next/server";
 
 export default async function Home() {
+  await connection();
   const user = await findUserByUserId("ad1b2ae0-30ac-818f-c44b-d041440369d5");
   if (!user) return <div className="p-5">user not found</div>;
 
