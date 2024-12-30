d784159847a83038dcb757fceef7274faa4ae0f1
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-31 00:07:27 +0900

    terraform コマンドをdocker化

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -3,4 +3,10 @@
 project_root=$(dirname "$0")/..
 env=$1
 shift
-terraform -chdir=$project_root/infra/environments/$env $@ -var-file=../../globals/common.tfvars
+
+docker run --rm -it \
+    -v "$project_root/infra:/infra" \
+    -v ~/.config/gcloud/application_default_credentials.json:/key.json \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/key.json \
+    hashicorp/terraform:latest -chdir=/infra/environments/$env \
+    $@ -var-file=../../globals/common.tfvars
