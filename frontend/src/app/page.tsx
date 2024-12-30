aa3acd9e99b40e89c19afe251782dea24aca2ce4
kanaru-ssk <kanaru.ssk@gmail.com>
2024-12-30 20:47:33 +0900

    不要なリンク削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <div className="flex flex-col items-start">
-      <Link className="underline" href="/251f5893-8560-464a-9c3b-542caed0297d">
-        サンプルユーザー
-      </Link>
       <Link className="underline" href="/signin">
         サインイン
       </Link>
