503328c353b414ac956458324a4ed15ffe91cfbe
kanaru-ssk <kanaru.ssk@gmail.com>
2025-01-01 20:58:10 +0900

    DB接続処理のコメントアウト解除

--- backend/cmd/grpc-server/main.go
+++ backend/cmd/grpc-server/main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 
 	"github.com/kanaru-ssk/payment-manager/backend/config"
+	"github.com/kanaru-ssk/payment-manager/backend/infrastructure/cloudsql"
 	"github.com/kanaru-ssk/payment-manager/backend/infrastructure/persistence"
 	"github.com/kanaru-ssk/payment-manager/backend/interface/grpcservice"
 	pb "github.com/kanaru-ssk/payment-manager/backend/interface/proto/user/v1"
@@ -24,12 +25,11 @@ func main() {
 	}
 	s := grpc.NewServer()
 
-	// cloudSqlClient, err := cloudsql.NewClient(config.DatabaseUrl)
-	// if err != nil {
-	// 	log.Fatalf("main.main err: %v", err)
-	// }
-	// userRepository := persistence.NewUserRepository(cloudSqlClient)
-	userRepository := persistence.NewUserRepository()
+	cloudSqlClient, err := cloudsql.NewClient(config.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("main.main err: %v", err)
+	}
+	userRepository := persistence.NewUserRepository(cloudSqlClient)
 	userUseCase := usecase.NewUserUseCase(userRepository)
 	userService := grpcservice.NewUserService(userUseCase)
 	pb.RegisterUserServiceServer(s, userService)
