9840b847f73d24eeee075cfad4fb0bc448e46b92
kanaru-ssk <kanaru.ssk@gmail.com>
2025-01-11 18:08:57 +0900

    googleapis削除

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -6,8 +6,3 @@ protoc \
   --ts_out=import_style=commonjs,binary:frontend/src/infrastructure \
   proto/user/v1/*.proto \
   proto/paymentcategory/v1/*.proto
-
-protoc \
-  --ts_out=import_style=commonjs,binary:frontend/src/infrastructure \
-  --proto_path=./proto/googleapis \
-  proto/googleapis/google/cloud/identitytoolkit/v2/*.proto
